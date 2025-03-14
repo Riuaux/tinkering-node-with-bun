0b107aedf1cedbef6d36fee5b035715e64d74c78
Riuaux <agustin.fullstack@gmail.com>
2025-03-14 17:28:54 -0600

    Add middleware for validate user role

---
+++ middleware/authorization.ts
@@ -0,0 +1,21 @@
+import type { ServerResponse } from "http";
+import type { AuthenticatedRequest } from "./authentication";
+import type { User } from "../models";
+
+export const authorizeRoles = (...roles: string[]) => {
+  return async (
+    req: AuthenticatedRequest,
+    res: ServerResponse
+  ): Promise<boolean> => {
+    const userRole = (req.user as User).role;
+
+    if (!userRole || !roles.includes(userRole)) {
+      res.statusCode = 403;
+      res.end(JSON.stringify({ message: "Role forbidden" }));
+      return false;
+    }
+
+    return true;
+  };
+};
+
