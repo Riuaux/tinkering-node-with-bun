fc3704f9bcea6e5f1a40a873b56d692450e57028
Riuaux <agustin.fullstack@gmail.com>
2025-03-14 18:42:49 -0600

    Index server file and port

--- config.ts
+++ config.ts
@@ -1,5 +1,7 @@
 export const config = {
   jwtSecret: (process.env.JWT_SECRET as string) || "supersecret",
+  port: (process.env.PORT as string) || 4000,
 };
 
 export default config;
+
--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+import cors from "cors";
+import { createServer } from "http";
+import { authRouter, characterRouter } from "./routes";
+import config from "./config";
+
+const corsMiddleware = cors();
+
+const server = createServer(async (req, res) => {
+  corsMiddleware(req, res, async () => {
+    res.setHeader("Content-Type", "application/json");
+
+    try {
+      if (req.url?.startsWith("/auth")) {
+        await authRouter(req, res);
+      } else if (req.url?.startsWith("/character")) {
+        await characterRouter(req, res);
+      } else {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ message: "Endpoint Not Found" }));
+      }
+    } catch (_err) {
+      res.statusCode = 500;
+      res.end(JSON.stringify({ message: "Internal Server Error" }));
+    }
+  });
+});
+
+server.listen(config.port, () => {
+  console.log(`Server running on ${config.port}`);
+});
+
